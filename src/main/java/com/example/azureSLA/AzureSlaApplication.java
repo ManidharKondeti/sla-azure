fef291f4dc09367d1887a6c571768ffb0db262b2
ManidharKondeti <manidharbabukondeti@gmail.com>
2024-04-16 11:43:00 -0500

    vault changes

--- src/main/java/com/example/azureSLA/AzureSlaApplication.java
+++ src/main/java/com/example/azureSLA/AzureSlaApplication.java
@@ -2,7 +2,6 @@ package com.example.azureSLA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import com.azure.identity.ClientSecretCredential;
 import com.azure.identity.ClientSecretCredentialBuilder;
 import com.azure.security.keyvault.secrets.SecretClient;
@@ -11,33 +10,27 @@ import com.azure.security.keyvault.secrets.models.KeyVaultSecret;
 import com.microsoft.applicationinsights.attach.ApplicationInsights;
 
 @SpringBootApplication
-@EnableConfigurationProperties
 public class AzureSlaApplication {
-
-    public static void main(String[] args) {
-
-        // configureApplicationInsights();
-        ApplicationInsights.attach();
-
-        String tenantId = System.getenv("AZURE_TENANT_ID");
-        String clientId = System.getenv("AZURE_CLIENT_ID");
-        String clientSecret = System.getenv("AZURE_CLIENT_SECRET");
+	public static void main(String[] args) {
+        
+		//configureApplicationInsights();
+		ApplicationInsights.attach();
 
         String keyVaultName = "slavault";
         String keyVaultUri = "https://" + keyVaultName + ".vault.azure.net";
         ClientSecretCredential cc = new ClientSecretCredentialBuilder()
-                .tenantId(tenantId)
-                .clientId(clientId)
-                .clientSecret(clientSecret).build();
-
+                                    .tenantId("08d2293e-cece-4727-890b-cab1fadeed57")
+                                    .clientId("17b68e0d-04e3-47d9-9a0d-cf8c83203e59")
+                                    .clientSecret("wRC8Q~5Pj0M5k_CQBt3HG41Zbz7nekjqLhsj3bLl").build();
+        
         // Use the Azure credential to authenticate with Azure
         SecretClient secretClient = new SecretClientBuilder()
-                .vaultUrl(keyVaultUri)
-                .credential(cc)
-                .buildClient();
+            .vaultUrl(keyVaultUri)
+            .credential(cc)
+            .buildClient();
 
         // Retrieve the secret
-        KeyVaultSecret retrievedSecret = secretClient.getSecret("DBConnectionString");
+         KeyVaultSecret retrievedSecret = secretClient.getSecret("DBConnectionString");
 
         // Access the secret value
         String secretVal = retrievedSecret.getValue();
@@ -50,7 +43,7 @@ public class AzureSlaApplication {
         System.setProperty("spring.datasource.username", secretValues[1].split("=")[1]);
         System.setProperty("spring.datasource.password", secretValues[2].split("=")[1]);
 
-        SpringApplication.run(AzureSlaApplication.class, args);
-
-    }
+		SpringApplication.run(AzureSlaApplication.class, args);
+		
+	}
 }
